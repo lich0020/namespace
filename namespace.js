c0ec148277d694349861bf0bf2660fe49e26ff20
Jeff Lichty <lich0020@algonquinlive.com>
2018-10-21 09:24:59 -0400

    playing with appending

--- namespace.js
+++ namespace.js
@@ -4,15 +4,20 @@ document.addEventListener("DOMContentLoaded", init);
 function init() {
 
 
-    var LICH0020 = document.createElement("div");
-//    LICH0020.className = "box";
-//    LICH0020.textContent = "lich0020";
-//
-//    
-//    let boxes = document.getElementById("boxes");
-//    boxes.appendChild(LICH0020);
+    var LICH0020 = document.createElement("div"); //creates div
+    LICH0020.className = "box";                     // gives div class name box
+    LICH0020.textContent = "lich0020";              // text lich0020 is inserted into div
+    
+
+    let boxes = document.getElementById("boxes"); //create id boxes
+    boxes.appendChild(LICH0020);                  //sets boxes within the div named LICH0020
 
 
+    
+    
+    
+    
+    
     LICH0020.addEventListener("click", mouseClick);
     LICH0020.addEventListener("mouseover", mouseOver);
     LICH0020.addEventListener("mouseout", mouseOut);
@@ -20,16 +25,15 @@ function init() {
 }
 
 function mouseClick() {
-    this.style.borderColor = "black";
-    this.style.backgroundColor = "green";
+    this.style.borderColor = "black";      //when clicked border turns black
+    this.style.backgroundColor = "green"; //when clicked fill turns green
 }
 
 function mouseOver(e) {
-    e.target.classList.toggle("highlight");
-
+    e.target.classList.toggle("highlight");   //toggles css highlight when moused over
 }
 
 function mouseOut(e) {
-    e.target.classList.toggle("highlight");
-    e.target.removeAttribute('style');
+    e.target.classList.toggle("highlight"); //toggles css highlight when moused out
+    e.target.removeAttribute('style'); //removes any styling to the box
 }
